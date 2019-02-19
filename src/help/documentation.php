82bfee35d902ac6845113e4ba55f3f9ce602f9ec
Layne Faler <laynefaler@gmail.com>
2019-02-18 23:00:13 -0800

    Updates to documentation for full string with dynamic properties

--- src/help/documentation.php
+++ src/help/documentation.php
@@ -3,12 +3,18 @@
 class DocumentationHelpCommand 
 {
 
-    private $ascii_art = "";
+    private $ascii_art = "     _                               _             
+    | |                             | |  _     _   
+    | |     __ _ _ __ __ ___   _____| |_| |_ _| |_ 
+    | |    / _` | '__/ _` \\ \\ / / _ \\ |_   _|_   _|
+    | |___| (_| | | | (_| |\\ V /  __/ | |_|   |_|  
+    \\_____/\\__,_|_|  \\__,_| \\_/ \\___|_|            
+                                                  
+    ";
 
     public function run($description, $version) {
 
 echo "{$this->ascii_art}
-
 Laravel Plus Plus Version: {$version}
 
 Laravel Plus Plus: Command Line Interface to make your life easier.
@@ -16,6 +22,7 @@ Laravel Plus Plus: Command Line Interface to make your life easier.
 => Use '--help' on any of the commands listed below for more details.
 
 List of commands:
-{$description}";
+{$description}
+";
     }
 }
