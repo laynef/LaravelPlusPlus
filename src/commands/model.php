daee4b7b0edb5eccd5bbc001cd635269dc5d9116
Layne Faler <laynefaler@gmail.com>
2019-02-19 09:27:11 -0800

    Added model and migration in templates

--- src/commands/model.php
+++ src/commands/model.php
@@ -9,16 +9,33 @@ class ModelCommand
         $script_line = implode($arguments, ' ');
 
         $dir = getcwd();
-        shell_exec("{$dir}/artisan make:model " . $model . ' ' . $script_line . '--migration --controller --resource --api');
+        exec("/usr/bin/php artisan make:model " . $model . ' ' . $script_line . '--migration --controller --resource --api');
 
         $model_regex = '/{{ MODEL }}/';
+        $model_lc_regex = '/{{ MODEL_LC_PLURAL }}/';
+
+        $model_lc = $this->pluralize($model);
+        $model_lc_plural = $this->snakeCase($model_lc);
 
         $controller_path = getcwd() . '/app/Http/Controllers/' . $model . 'Controller.php';
+        $model_path = getcwd() . '/app/' . $model . '.php';
+        $timestamp = date('Y_m_d_His');
+        $migration_name = $timestamp . '_create_' . $model_lc_plural . '_table';
+        $migration_path = getcwd() . '/database/migrations/' . $migration_name . '.php';
 
         $template_str = file_get_contents(__DIR__ . '/../templates/controller.txt');
+        $model_template_str = file_get_contents(__DIR__ . '/../templates/model.txt');
+        $migration_template_str = file_get_contents(__DIR__ . '/../templates/migration.txt');
 
         $new_controller_string = preg_replace($model_regex, $model, $template_str);
+        $new_model_string = preg_replace($model_lc_regex, $model_lc_plural, $model_template_str);
+        $new_model_string = preg_replace($model_regex, $model, $new_model_string);
+        $new_migration_string = preg_replace($model_regex, $model, $migration_template_str);
+        $new_migration_string = preg_replace($model_lc_regex, $model_lc_plural, $new_migration_string);
+
         file_put_contents($controller_path, $new_controller_string);
+        file_put_contents($model_path, $new_model_string);
+        file_put_contents($migration_path, $new_migration_string);
 
         echo "Your controller was generated.";
     }
@@ -32,5 +49,23 @@ class ModelCommand
         $str = strtolower(substr($str,0,1)).substr($str,1);
         return ucfirst($str);
     }
+
+    public function snakeCase($str) {
+        $str[0] = strtolower($str[0]);
+        $func = create_function('$c', 'return "_" . strtolower($c[1]);');
+        return preg_replace_callback('/([A-Z])/', $func, $str);
+    }
+
+    public static function pluralize($string) {
+        $last_letter = strtolower($string[strlen($string)-1]);
+        switch($last_letter) {
+            case 'y':
+                return substr($string,0,-1).'ies';
+            case 's':
+                return $string.'es';
+            default:
+                return $string.'s';
+        }
+}
 
 }
