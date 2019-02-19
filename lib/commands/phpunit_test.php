4e52d1d4ae26571f714c1f4f7463da1671ec2377
Layne Faler <laynefaler@gmail.com>
2019-02-18 20:20:36 -0800

    Initial start on phpunit test generations

--- lib/commands/phpunit_test.php
+++ lib/commands/phpunit_test.php
@@ -1,14 +1,17 @@
 <?php
 
-$route = $route[1];
+$resource_name = $argv[1];
 
 $route_regex = '/{{ RESOURCE_ROUTE }}/';
+$route_capitalized_regex = '/{{ RESOURCE_ROUTE_CAPTIALIZE }}/';
+$route_captialized = ucfirst($resource_name);
 
-$test_path = __DIR__ . '/../test/Feature' . $route . 'Test.php';
+$test_path = __DIR__ . '/../test/Feature/' . $route_captialized . 'Test.php';
 
-$template_str = file_get_contents(__DIR__ . '/../templates/php_unit_test.txt');
+$template_str = file_get_contents(__DIR__ . '/templates/php_unit_test.txt');
 
-$new_test_string = preg_replace($route_regex, $route, $template_str);
+$new_test_string = preg_replace($route_regex, $resource_name, $template_str);
+$new_test_string = preg_replace($route_capitalized_regex, $route_captialized, $new_test_string);
 file_put_contents($test_path, $new_test_string);
 
-echo "Your controller was generated.";
+echo "Your test was generated.";
